53f08b683627de3af7443bd9cbea5c2cd74a3535
Wouter Versyck <wouter.versyck@gmail.com>
2019-01-12 20:46:40 +0100

    Added userController integration test

--- src/test/java/be/wouterversyck/graphqlpoc/web/controllers/UserControllerIntegrationTest.java
+++ src/test/java/be/wouterversyck/graphqlpoc/web/controllers/UserControllerIntegrationTest.java
@@ -4,13 +4,13 @@ import be.wouterversyck.graphqlpoc.domain.models.User;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class HomeControllerIntegrationTest extends AbstractIntegrationTest{
+public class UserControllerIntegrationTest extends AbstractIntegrationTest{
 
     @Test
     public void addUserShouldReturnSameUser() {
@@ -41,8 +41,8 @@ public class HomeControllerIntegrationTest extends AbstractIntegrationTest{
                 .get("/user/1")
                 .as(User.class);
 
-        assertThat(response.getFirstName(), equalTo("first_name"));
-        assertThat(response.getLastName(), equalTo("last_name"));
+        assertThat(response.getFirstName(), equalTo("first_name_1"));
+        assertThat(response.getLastName(), equalTo("last_name_1"));
     }
 
     @Test
@@ -55,10 +55,40 @@ public class HomeControllerIntegrationTest extends AbstractIntegrationTest{
                 .get("/user/1/2")
                 .as(User[].class);
 
-        assertThat(response[0].getFirstName(), equalTo("first_name_3"));
-        assertThat(response[0].getLastName(), equalTo("last_name_3"));
+        assertThat(response[0].getFirstName(), equalTo("first_name_2"));
+        assertThat(response[0].getLastName(), equalTo("last_name_2"));
 
-        assertThat(response[0].getFirstName(), equalTo("first_name_4"));
-        assertThat(response[0].getLastName(), equalTo("last_name_4"));
+        assertThat(response[1].getFirstName(), equalTo("first_name_3"));
+        assertThat(response[1].getLastName(), equalTo("last_name_3"));
+    }
+
+    @Test
+    public void beanValidationWorks() {
+        var user1 = User.builder()
+                .withFirstName("firstName")
+                .build();
+
+        var user2 = User.builder()
+                .withLastName("lastName")
+                .build();
+
+        for(User user : List.of(user1, user2)) {
+            givenWithAuth()
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .body(user)
+                    .when()
+                    .post("/user")
+                    .then()
+                    .statusCode(400);
+        }
+    }
+
+    @Test
+    public void userEndpointIsSecured() {
+        given()
+                .get("/user")
+                .then()
+                .statusCode(401);
     }
 }
