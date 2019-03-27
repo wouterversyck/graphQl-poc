981f39d60600d99ff837db4d87b727bde4250f69
Wouter Versyck <wouter.versyck@designisdead.com>
2019-03-27 18:47:27 +0100

    Fix build

--- src/main/resources/webapp/src/app/users/components/add-user/add-user.component.ts
+++ src/main/resources/webapp/src/app/users/components/add-user/add-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserRestService } from '../../services/rest/user-rest.service';
 import { LoaderService } from '../../../core/components/loader/service/loader.service';
-import { finalize } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, finalize, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-user',
@@ -13,12 +13,25 @@ export class AddUserComponent implements OnInit {
   userForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
+    userName: new FormControl('', Validators.required)
   });
   constructor(private userService: UserRestService, private loaderService: LoaderService) { }
 
   ngOnInit() {
+    this.userForm.valueChanges
+      .pipe(
+        map(e => e.userName),
+        debounceTime(400),
+        distinctUntilChanged()
+      ).subscribe(e => this.userService.userNameExists(e).subscribe(result => {
+        if (result) {
+          this.userNameControl.setErrors({
+            'already.exists': true
+          });
+        }
+        this.userForm.updateValueAndValidity();
+    }));
   }
-
   onSubmit() {
     this.loaderService.show();
     this.userService.addUser(this.userForm.getRawValue())
@@ -26,5 +39,17 @@ export class AddUserComponent implements OnInit {
         finalize(() => this.loaderService.hide())
       )
       .subscribe(() => this.userForm.reset());
+  }
+
+  get firstNameControl() {
+    return this.userForm.controls['firstName'];
+  }
+
+  get lastNameControl() {
+    return this.userForm.controls['lastName'];
+  }
+
+  get userNameControl() {
+    return this.userForm.controls['userName'];
   }
 }
